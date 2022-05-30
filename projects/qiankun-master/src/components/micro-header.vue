982f0265cec65dcee6060a790411c031dd8ec4d9
zhangcheng <2778162192@qq.com>
2022-05-30 16:24:53 +0800

    qiankun

--- projects/qiankun-master/src/components/micro-header.vue
+++ projects/qiankun-master/src/components/micro-header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="micro-header">
-        <div class="micro-title"><i>Mr ORANGE</i></div>
+        <div class="micro-title"><i>Mr ORANGE1</i></div>
         <digital-clock></digital-clock>
     </div>
 </template>
